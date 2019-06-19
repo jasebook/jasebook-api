634fece0b5e8e44950f2be830895d00638e32905
Anthony Hull <m@anthonyhull.me>
2019-06-19 16:07:43 +0100

    fixing package declarations

--- src/test/java/com/jasebook/app/test/AppTests.java
+++ src/test/java/com/jasebook/app/test/AppTests.java
@@ -1,7 +1,10 @@
+package com.jasebook.app.test;
+
+import com.jasebook.app.App;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = App.class)
 class AppTests {
 
     @Test
