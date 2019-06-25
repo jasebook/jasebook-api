46f747f6f070b7540ea11903b1a824f6889039e1
Anthony Hull <m@anthonyhull.me>
2019-06-25 11:37:06 +0100

    removed unnecessary includes and dependancies

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -1,16 +1,11 @@
 package com.makersacademy.acebook.controller;
 
-import com.makersacademy.acebook.repository.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class HomeController {
-
-	@Autowired
-	private PostRepository postRepository;
 
 	@RequestMapping(value = "/")
 	public String index() {
