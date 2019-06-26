fdd8508e2cee16ecad4ca1f002bf5988c62fe7e7
Geoff Smith <smith260194@gmail.com>
2019-06-26 17:17:46 +0100

    Fixing test

--- Cypress/integration/viewPostsFeature.spec.js
+++ Cypress/integration/viewPostsFeature.spec.js
@@ -4,7 +4,8 @@ describe('Mainpage', () => {
     it('allows you to post a post and it is displays', function () {
         cy.visit('http://localhost:8080/')
         cy.get('[data-cy=addPostTxtBox]').type('Ant and Anna were here')
-        cy.get('[data-cy=submitPost]').click()
+        cy.get('[data-cy=submitPost]').click();
+        cy.wait(1000);
         cy.get('[cy-data="postsContainer"] > :nth-child(1)').contains('Ant and Anna were here')
     });
 });
