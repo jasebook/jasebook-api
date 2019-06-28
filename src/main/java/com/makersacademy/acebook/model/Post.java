3b34d727fc4bc45727e1d604dc56c2aa5f613b7e
Anna <annas.wieclaw@gmail.com>
2019-06-28 11:03:52 +0100

    Change test back to dev

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -2,18 +2,20 @@ package com.makersacademy.acebook.model;
 
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Data
 @Entity
-@Table(name = "POSTS")
+@Table(name="POSTS")
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+//    @Column(name = "user_id", nullable=false)
     private Long user_id;
 
     @ManyToOne(optional=false)
